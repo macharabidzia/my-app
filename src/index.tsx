7cea2615a8af4baae0b00d4ca5cdf7842cc18962
Giorgi Matcharashvili <giorgi.matcharashvili@flatrocktech.com>
2021-02-20 17:42:04 +0400

    implemented routing and added redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Router from './router';
+import { Provider } from 'react-redux';
+import store from './core/store';
 import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  </Provider>
 );
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/router/index.tsx
@@ -0,0 +1,12 @@
+import { Switch, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+
+const Router = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+    </Switch>
+  );
+};
+
+export default Router;
